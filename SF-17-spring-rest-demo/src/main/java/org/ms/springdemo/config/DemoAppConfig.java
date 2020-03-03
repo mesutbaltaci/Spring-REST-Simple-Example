c3dccbd7a6db0060663d79670e5add9abbc10be7
mesutbaltaci <mesutbaltaci@hotmail.com>
2020-03-03 12:51:43 -0500

    Spring Rest Rest controller says hello

---
+++ SF-17-spring-rest-demo/src/main/java/org/ms/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package org.ms.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.ms.springdemo")
+public class DemoAppConfig {
+
+}
---
+++ SF-17-spring-rest-demo/src/main/java/org/ms/springdemo/config/MySpringMvcDispatcherServetInitializer.java
@@ -0,0 +1,25 @@
+package org.ms.springdemo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServetInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {DemoAppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] {"/"};
+	}
+
+}
